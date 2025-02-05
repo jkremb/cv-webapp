0a24492ebea74d2a9b067f832e9572156c202848
Julian Kremb <julian.dschana.kremb@gmail.com>
2025-02-05 23:12:27 +0100

    SPA filled out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cv-webapp';
+  isHeaderVisible = true;
+  lastScrollPosition = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const currentScroll = window.pageYOffset;
+    this.isHeaderVisible = currentScroll < this.lastScrollPosition || currentScroll < 50;
+    this.lastScrollPosition = currentScroll;
+  }
 }
